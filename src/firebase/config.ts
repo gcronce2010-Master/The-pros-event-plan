e3e72aa68cbbd72b3f2295edbde52396b58f3529
Grant Cronce <gcronce2010@gmail.com>
2026-02-12 02:56:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1634096656-7cb09",
+  "appId": "1:858826101053:web:29679d0e1f9bd54138b554",
+  "apiKey": "AIzaSyAKTl9aZ81Xk3HoXJOJ2wZAcr2L6XvMpKM",
+  "authDomain": "studio-1634096656-7cb09.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "858826101053"
+};
